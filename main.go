5a93abe2919ec62e4d1187abba93f42a1d567397
Sergio Rivas <sergiorivas@gmail.com>
2019-10-04 23:47:52 -0400

    Refactor main

--- main.go
+++ main.go
@@ -16,13 +16,14 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func main() {
-
+func loadEnv() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
 
+func connectToDB() *gorm.DB {
 	connectionString := fmt.Sprintf(
 		"host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
 		os.Getenv("DB_HOST"),
@@ -35,18 +36,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	return db
+}
 
-	db.LogMode(!(os.Getenv("PRODUCTIO") != "" && os.Getenv("PRODUCTION") == "TRUE")) //For debugging
-
+func loadSchema() string {
 	s, err := api.GetSchema("./api/schema.graphql")
 	if err != nil {
 		panic(err)
 	}
 
+	return s
+}
+
+func main() {
+	loadEnv()
+
+	var db *gorm.DB
+	db = connectToDB()
+	defer db.Close()
+
+	db.LogMode(!(os.Getenv("PRODUCTIO") != "" && os.Getenv("PRODUCTION") == "TRUE")) //For debugging
+
+	// Preparing schema
 	rootResolver := resolvers.RootResolver{DB: db}
-	schema := graphql.MustParseSchema(s, &rootResolver)
+	schema := graphql.MustParseSchema(loadSchema(), &rootResolver)
+
 	http.Handle("/graphql", &relay.Handler{Schema: schema})
 	log.Fatal(http.ListenAndServe(":3001", nil))
-
 }
